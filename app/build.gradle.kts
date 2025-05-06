1a76e33cf912aed174d5648398d2812002f893bf
mnepach <mnepach@gmail.com>
2025-05-06 15:06:41 +0300

    dos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,34 @@ android {
 }
 
 dependencies {
-
+    // AndroidX и Material Design
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+
+    // Room для работы с базой данных
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-rxjava3:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+
+    // RxJava3
+    implementation("io.reactivex.rxjava3:rxjava:3.1.8")
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
+
+    // Retrofit для сетевых запросов
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava3:2.9.0")
+
+    // OkHttp для HTTP-клиента и логирования
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    // Gson для парсинга JSON
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // Тестирование
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
